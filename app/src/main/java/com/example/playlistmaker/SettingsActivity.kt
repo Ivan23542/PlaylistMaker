1d10f6683ccffd9cba99d3af96f9105ef88c4a74
Admin <vanalapkovskij09853@gmail.com>
2025-09-15 18:49:19 +0300

    Окончательная версия дз

--- app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
@@ -3,6 +3,7 @@ package com.example.playlistmaker
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageButton
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -12,6 +13,8 @@ class SettingsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+        setContentView(R.layout.activity_settings)
+        val BackButton = findViewById<ImageButton>(R.id.back)
         BackButton.setOnClickListener {
 
             val intent = Intent(this, MainActivity::class.java)
