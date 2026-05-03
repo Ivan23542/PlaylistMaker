c21aaf35874563337ff5ab5e42047667a9fb9b43
Ivan <russia09853@gmail.com>
2026-05-03 12:11:27 +0300

    sprint_18

--- app/src/main/java/com/example/playlistmaker/di/ViewModelModule.kt
+++ app/src/main/java/com/example/playlistmaker/di/ViewModelModule.kt
@@ -3,6 +3,8 @@ package com.example.playlistmaker.di
 import com.example.playlistmaker.domain.model.Track
 import com.example.playlistmaker.presentation.main.MainViewModel
 import com.example.playlistmaker.presentation.media.MediatekaViewModel
+import com.example.playlistmaker.presentation.media.favorites.FavoriteTracksViewModel
+import com.example.playlistmaker.presentation.media.playlists.PlaylistsViewModel
 import com.example.playlistmaker.presentation.player.PlayerViewModel
 import com.example.playlistmaker.presentation.playlist.NewPlaylistViewModel
 import com.example.playlistmaker.presentation.search.SearchViewModel
@@ -15,6 +17,10 @@ val viewModelModule = module {
     viewModel { MainViewModel() }
 
     viewModel { MediatekaViewModel() }
+
+    viewModel { FavoriteTracksViewModel() }
+
+    viewModel { PlaylistsViewModel() }
 
     viewModel { SearchViewModel(get(), get()) }
 
